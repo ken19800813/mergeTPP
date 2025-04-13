1530302deb9d19b831985206672dae561f17fd0f
KenH0813 <ken.huang@hcinnovation.tw>
2025-04-13 11:50:14 +0800

    0413-2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1744509704|1757949990';
+const CACHE_VERSION = '1744516202|8255781921';
 /** @type {string} */
 const CACHE_PREFIX = '集氣救阿北-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
